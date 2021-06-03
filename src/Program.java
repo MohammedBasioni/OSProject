f10526bea9fb6d4ce7977ee91b6e1e79ad00e064
someone <mohammedalnamaki@gmail.com>
2021-06-03 20:55:12 +0200

    end of MS1's coding!

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -12,21 +9,28 @@ public class Program {
 
 
     private HashMap<String,String> variables;
+    private String filePath;
 
-    public Program() {
+    public Program(String filePath) {
+        this.filePath = filePath;
         this.variables = new HashMap<>();
     }
 
     // assign instruction
     public void assign(String name, Object value) throws OSException {
             validateValueIsString(value);
-            variables.put(name, (String) value);
+        if (variables.containsKey(value))
+            value = variables.get(value);
+        variables.put(name, (String) value);
     }
+
     // print instruction
-    public void print(String name) throws OSException {
-            validateExistingVariable(name);
-            System.out.println(variables.get(name));
+    public void print(String data) throws OSException {
+        if (variables.containsKey(data))
+            data = variables.get(data);
+        System.out.println(data);
     }
+
     // add instruction
     public void add(String variable1, String variable2) throws OSException {
         validateExistingVariable(variable1);
@@ -39,21 +43,46 @@ public class Program {
     public void writeFile(String fileName, String data) throws IOException {
         if (variables.containsKey(data))
             data = variables.get(data);
-        FileWriter file = new FileWriter(fileName +".txt");
+        if (variables.containsKey(fileName))
+            fileName = variables.get(fileName);
+        FileWriter file = new FileWriter(fileName);
         file.write(data);
         file.close();
     }
 
     public String readFile(String fileName) throws IOException {
         String data = "";
-        data = new String(Files.readAllBytes(Paths.get(fileName+".txt")));
+        if (variables.containsKey(fileName))
+            fileName = variables.get(fileName);
+        data = new String(Files.readAllBytes(Paths.get(fileName)));
         return data;
     }
 
-
-
-
-
+    public void run() throws IOException, OSException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        while (br.ready()){
+            String[] line = br.readLine().split(" ");
+            if (line[0].equals("assign")){
+                String data = "";
+                if (line[2].equals("input"))
+                    data = sc.nextLine();
+                else if (line[2].equals("readFile"))
+                    data = readFile(line[3]);
+                else
+                    data = line[2];
+                assign(line[1],data);
+            }
+            else if (line[0].equals("print"))
+                print(line[1]);
+            else if (line[0].equals("add"))
+                add(line[1],line[2]);
+            else if (line[0].equals("writeFile"))
+                writeFile(line[1],line[2]);
+            else if (line[0].equals("readFile"))
+                readFile(line[1]);
+        }
+    }
 
     // Validations
     public void validateExistingVariable(String name) throws OSException {
@@ -77,10 +106,15 @@ public class Program {
         return result;
     }
 
-    // Getters and Setters
+    public static void main(String[] args) throws IOException, OSException {
+        Program program1 = new Program("Program 1.txt");
+        program1.run();
+        Program program2 = new Program("Program 2.txt");
+        program2.run();
+        Program program3 = new Program("Program 3.txt");
+        program3.run();
 
-    public HashMap<String, String> getVariables() {
-        return variables;
     }
+
 
 }
